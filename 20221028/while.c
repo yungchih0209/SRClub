87cac707e21f172d24f5c885404519a5ed24ebe4
yungchih0209 <chih105015@gmail.com>
2022-10-26 22:49:26 +0800

    Update while.c

--- 20221028/while.c
+++ 20221028/while.c
@@ -1,8 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
+/*輸入一個整數 N,d ，輸出0到N的等差數列。*/
+/*使用DoWhile使最後不會印出逗號*/
 
 int main(){
-    while(1){
-        
+    int n,d;
+    printf("Input number:");
+    scanf("%d", &n);
+    scanf("%d",&d);
+
+    int first = 0;
+    while(first <= n){
+        printf(",%d", first);
+        first+=d;
     }
     return 0;
 }
