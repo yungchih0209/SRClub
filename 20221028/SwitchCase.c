b07ea2ae75db6d684752da0e70f11f24a9194bbe
yungchih0209 <chih105015@gmail.com>
2022-10-26 22:49:23 +0800

    Update SwitchCase.c

--- 20221028/SwitchCase.c
+++ 20221028/SwitchCase.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
-/*練習*/
+/*練習判斷是否*/
 int main(){
     int x;
     scanf("%d",&x);
     switch(x){
         case 1:
-            printf("星期一");
-            break;
         case 2:
-            printf("星期二");
-            break;
         case 3:
-            printf("星期三");
+        case 4:
+        case 5:
+            printf("平日");
+            break;
+        case 6:
+        case 7:
+            printf("開心的假日");
             break;
         default:
-            printf("Default!");
+            printf("輸入錯誤!");
+            break;
     }
     return 0;
 }
